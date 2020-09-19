2f4f809f7ebd2677845564bb001159061f109ed3
Enrique <emontas@mit.edu>
2020-09-19 16:38:09 -0400

    parse server

---
+++ app/src/main/java/com/example/hackapp2020/application/Keys.java
@@ -0,0 +1,15 @@
+package com.example.hackapp2020.application;
+
+public class Keys {
+    public static final String appId = "hack-mit2020";
+    public static final String masterKey = "bavenBoward";
+
+    public static String getAppId() {
+        return appId;
+    }
+
+    public static String getMasterKey() {
+        return masterKey;
+    }
+
+}
