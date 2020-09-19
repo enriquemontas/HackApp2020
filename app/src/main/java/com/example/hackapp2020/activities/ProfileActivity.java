db517d9bc735f020caaa7072b4558ba8072feabf
Enrique <emontas@mit.edu>
2020-09-19 19:02:29 -0400

    redid fragment layout to activity

--- app/src/main/java/com/example/hackapp2020/activities/ProfileActivity.java
+++ app/src/main/java/com/example/hackapp2020/activities/ProfileActivity.java
@@ -6,37 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.hackapp2020.R;
 import com.example.hackapp2020.databinding.ActivityMainBinding;
+import com.example.hackapp2020.databinding.ActivityProfileBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class MainActivity extends AppCompatActivity{
+public class ProfileActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        final ActivityProfileBinding binding = ActivityProfileBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
         final FloatingActionButton foaLeft = binding.left;
-        final FloatingActionButton foaRight = binding.right;
 
         foaLeft.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, LeaderboardActivity.class);
+                Intent i = new Intent(ProfileActivity.this, MainActivity.class);
                 startActivity(i);
             }
         });
 
-        foaRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(i);
-            }
-        });
+
     }
-
 }
